5d738086c471812c004c7e73c5e7ec6d67cdba8e
冯程普 <fengchengpu@fengchengpu.local>
2019-07-15 17:59:04 +0800

    remove empity import

--- src/containers/Root.prod.jsx
+++ src/containers/Root.prod.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import { Route, Router} from 'react-router';
 import RouterTree from '../router/routes'
 
 export default class Root extends Component {
